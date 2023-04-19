8912e82149ac28a9e95358c3e3d6428bac242123
Abou FOFANA <abou.fofana-ext@ouidou.fr>
2023-04-19 11:17:33 +0200

    add gitignore

--- src/main/java/org/design/pattern/Duck.java
+++ src/main/java/org/design/pattern/Duck.java
@@ -18,4 +18,12 @@ public abstract class Duck {
     public void swim() {
         System.out.println("All duck float, even decoy!");
     }
+
+    public void setFlyBehavior(FlyBehavior flyBehavior) {
+        this.flyBehavior = flyBehavior;
+    }
+
+    public void setQuackBehavior(QuackBehavior quackBehavior) {
+        this.quackBehavior = quackBehavior;
+    }
 }
